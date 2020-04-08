21f2e53a3469742dc902b9d13032fac60a2df864
Oliver Szabo <oleewere@gmail.com>
2020-04-08 14:41:50 +0200

    CB-6380. Do not persist telemetry component for stack with template type.

--- core/src/main/java/com/sequenceiq/cloudbreak/converter/v4/stacks/StackV4RequestToStackConverter.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/converter/v4/stacks/StackV4RequestToStackConverter.java
@@ -139,8 +139,9 @@ public class StackV4RequestToStackConverter extends AbstractConversionServiceAwa
         stack.setInstanceGroups(convertInstanceGroups(source, stack));
         measure(() -> updateCluster(source, stack, workspace),
                 LOGGER, "Converted cluster and updated the stack in {} ms for stack {}", source.getName());
-        stack.getComponents().add(getTelemetryComponent(stack, source));
-
+        if (!isTemplate(source)) {
+            stack.getComponents().add(getTelemetryComponent(stack, source));
+        }
         stack.setGatewayPort(source.getGatewayPort());
         stack.setUuid(UUID.randomUUID().toString());
         stack.setType(source.getType());
